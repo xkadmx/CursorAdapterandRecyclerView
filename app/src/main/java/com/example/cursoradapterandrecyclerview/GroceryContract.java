9fe38cc11691ee256e498053486ef482bba0b68f
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-05 21:37:17 +0200

    creating new class GroceryContract with public static class GroceryEntry implementing BaseColumn interface

---
+++ app/src/main/java/com/example/cursoradapterandrecyclerview/GroceryContract.java
@@ -0,0 +1,10 @@
+package com.example.cursoradapterandrecyclerview;
+
+import android.provider.BaseColumns;
+
+public class GroceryContract {
+
+    public static final class GroceryEntry implements BaseColumns{
+        public static final String TABLE_NAME = "groceryList";
+    }
+}
