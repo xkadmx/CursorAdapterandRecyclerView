070d188dad6e4d44253d723d943b0f1b47eb489f
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-05 21:43:56 +0200

    creating the table method containing the values we will add

--- app/src/main/java/com/example/cursoradapterandrecyclerview/GroceryContract.java
+++ app/src/main/java/com/example/cursoradapterandrecyclerview/GroceryContract.java
@@ -4,7 +4,12 @@ import android.provider.BaseColumns;
 
 public class GroceryContract {
 
+    private GroceryContract(){} // instanceof the class that we will never need
+
     public static final class GroceryEntry implements BaseColumns{
-        public static final String TABLE_NAME = "groceryList";
+        public static final String TABLE_NAME = "groceryList";    // name of the whole table
+        public static final String COLUMN_NAME = "name";          // name of the first column of the table
+        public static final String COLUMN_AMOUNT = "amount";
+        public static final String TIME_STAMP = "timestamp";
     }
 }
