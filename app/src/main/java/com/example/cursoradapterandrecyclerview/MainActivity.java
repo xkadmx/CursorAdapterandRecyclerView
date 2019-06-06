9e2309d79a55c23f0a42bb758bb5bac267806c49
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-06 21:26:14 +0200

    setting onClickListeners to the button (TODO build the method for each)

--- app/src/main/java/com/example/cursoradapterandrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/cursoradapterandrecyclerview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.cursoradapterandrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditTextName = findViewById(R.id.editText_name);
+        mTextViewAmount = findViewById(R.id.textView_amount);
+
+        Button buttonIncrease = findViewById(R.id.button_increase);
+        Button buttonDecrease = findViewById(R.id.button_decrease);
+        Button buttonAdd = findViewById(R.id.button_add);
+
+        buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increase(); //TODO build the method increase
+            }
+        });
+        buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decrease(); //TODO build the method decrease
+            }
+        });
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();   // TODO build the method addItem
+            }
+        });
     }
 }
