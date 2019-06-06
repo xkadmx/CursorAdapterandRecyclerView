766bc8a0dd730bc782080a0fde3c664b55594ea9
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-06 20:40:21 +0200

    inserting widgets in MainActivity

--- app/src/main/java/com/example/cursoradapterandrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/cursoradapterandrecyclerview/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.cursoradapterandrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEditTextName;
+    private TextView mTextViewAmount;
+    private int mAmount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
